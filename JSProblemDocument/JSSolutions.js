0d9d54554b5d64d0ecddbb9baf548236a1e541d2
ayushnvs <ayush.mishra.ald@gmail.com>
2022-10-01 22:05:00 +0530

    JSFuns

--- JSProblemDocument/JSSolutions.js
+++ JSProblemDocument/JSSolutions.js
@@ -41,6 +41,35 @@
     }
     return true
 }
-
 //TODO: (manage Time Complexity)
 // console.log(isPrime(961748927))
+
+/**
+ * Solution 3
+ *
+ * @param {number} n The number
+ * @returns {string} The text version of the number
+ */
+ function numberToText (n) {
+    let num = `${n}`
+    if (num.length == 2 && num[0]==1 && num[1]==0) {
+        return "Ten"
+    }
+    function Ones (x) {
+        let numList = ["","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
+        return numList[x]
+    }
+    function Tens(x) {
+        let numList = ["","","Twenty","Thirty","Fourty","Fifty","Sixty","Seventy","Eighty","Ninety"]
+        return numList[x]
+    }
+    if (num.length == 1) {
+        return Ones(num)
+    } else if (num.length == 2) {
+        return Tens(num[0]) + " " + Ones(num[1])
+    } else if (num.length == 3) {
+        return Ones(num[0]) + " Hundred " + Tens(num[1]) + " " + Ones(num[2])
+    }
+}
+//INCOMPLETE
+console.log(numberToText(390))
