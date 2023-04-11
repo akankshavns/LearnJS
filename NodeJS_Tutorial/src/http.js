55348258262c70d2396f8003bffb83a4d0716860
ayushnvs <ayush.mishra.ald@gmail.com>
2023-04-11 15:46:56 +0530

    http module

--- NodeJS_Tutorial/src/http.js
+++ NodeJS_Tutorial/src/http.js
@@ -1,13 +1,21 @@
 const http = require('http')
+const fs = require('fs')
 
-const server = http.createServer((req,res) => {
-    if (req.url === '/') {
-        res.write('Hello world from nodeJS')
-        res.end() // Important for sending response
-    } else {
-        res.write('Hellow again from nodeJS\nYou are not requesting from root domain.')
-        res.end()
-    }
-})
+// const server = http.createServer((req,res) => {
+//     if (req.url === '/') {
+//         res.write('Hello world from nodeJS')
+//         res.end() // Important for sending response
+//     } else {
+//         res.write('Hellow again from nodeJS\nYou are not requesting from root domain.')
+//         res.end()
+//     }
+// })
 
-server.listen('3000');
+// server.listen('3000');
+
+
+http.createServer((req, res) => {
+    const readStream = fs.createReadStream('./NodeJS_tutorial/src/exampleFiles/index.html')
+    res.writeHead(200, {'content-type': 'text/html'})
+    readStream.pipe(res)
+}).listen(3000)
